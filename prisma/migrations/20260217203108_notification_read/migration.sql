80d41bdd0d0da97192e01c0a1872cbd282664756
Thomas Mildner <kontakt@mildner-thomas.de>
2026-02-22 10:32:58 +0100

    refactor: remove unused columns from Document table and update Insurance deductible default

--- prisma/migrations/20260217203108_notification_read/migration.sql
+++ prisma/migrations/20260217203108_notification_read/migration.sql
@@ -1,18 +1,4 @@
-/*
-  Warnings:
 
-  - You are about to drop the column `filePath` on the `Document` table. All the data in the column will be lost.
-  - You are about to drop the column `fileSize` on the `Document` table. All the data in the column will be lost.
-  - You are about to drop the column `mimeType` on the `Document` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Document" DROP COLUMN "filePath",
-DROP COLUMN "fileSize",
-DROP COLUMN "mimeType";
-
--- AlterTable
-ALTER TABLE "Insurance" ALTER COLUMN "deductible" DROP DEFAULT;
 
 -- CreateTable
 CREATE TABLE "NotificationRead" (
